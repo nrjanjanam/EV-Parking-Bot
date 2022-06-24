2c25986fde0bdeaf0341c3be0e1ac8124baaec6e
Neeha Rathna Janjanam <Neeha.Janjanam@ril.com>
2022-06-25 00:41:56 +0530

    Added Micrsoft ID and Password for Echobot

--- index.js
+++ index.js
@@ -34,8 +34,8 @@ server.listen(process.env.port || process.env.PORT || 3978, () => {
 const credentialsFactory = new ConfigurationServiceClientCredentialFactory({
     MicrosoftAppId: process.env.MicrosoftAppId,
     MicrosoftAppPassword: process.env.MicrosoftAppPassword,
-    MicrosoftAppType: process.env.MicrosoftAppType,
-    MicrosoftAppTenantId: process.env.MicrosoftAppTenantId
+    // MicrosoftAppType: process.env.MicrosoftAppType,
+    // MicrosoftAppTenantId: process.env.MicrosoftAppTenantId
 });
 
 const botFrameworkAuthentication = createBotFrameworkAuthenticationFromConfiguration(null, credentialsFactory);
